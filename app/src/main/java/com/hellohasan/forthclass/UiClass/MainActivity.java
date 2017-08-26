a5766e1f6e412641fcd80a46d21b734ac80ada06
Hasan Abdullah <hasan_cse91@yahoo.com>
2017-08-26 12:43:28 +0600

    link added

--- app/src/main/java/com/hellohasan/forthclass/UiClass/MainActivity.java
+++ app/src/main/java/com/hellohasan/forthclass/UiClass/MainActivity.java
@@ -6,7 +6,7 @@
  - `ButterKnife` Library to reduce boilerplate code
  - Phone call
  - Run time permission for phone call
- - Send a POST request to a PHP server and receive a response
+ - Send a POST request to a PHP server and receive a response: https://hellohasan.com/2016/12/03/android-retrofit-get-post-method/
  - Update UI from another class using `interface`
  */
 
